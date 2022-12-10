890f02a827502a08488f73b0a0dc582c0697b596
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-09 22:21:51 -0400

    Update: the class attribute to className proper react syntax

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div class="jumbotron p-2 text-center">
-            <h1 class="display-4">Welcome to Intergalactic Digital</h1>
-            <p class="lead">Create Read Update Delete Blogs, Login and Logout. Users will have roles associated to them</p>
-            <hr class="my-4" />
+        <div className="jumbotron p-2 text-center">
+            <h1 className="display-4">Welcome to Intergalactic Digital</h1>
+            <p className="lead">Create Read Update Delete Blogs, Login and Logout. Users will have roles associated to them</p>
+            <hr className="my-4" />
             <p>We are going to be able to login and see blogs and then improve as we develope more!</p>
 
-            <button type="button" class="btn btn-primary btn-lg rounded-0">Learn more</button>
+            <button type="button" className="btn btn-primary btn-lg rounded-0">Learn more</button>
         </div>
     )
 }
