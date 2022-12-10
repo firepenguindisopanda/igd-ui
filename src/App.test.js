8444b7134a2cd93d5bb41cc384728a54f08cc7ee
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-09 21:36:51 -0400

    Updated: test script for App file

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders heading with Intergalactic Digital', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Intergalactic Digital/i);
   expect(linkElement).toBeInTheDocument();
 });
