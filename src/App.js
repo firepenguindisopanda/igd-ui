340992dd91449c0145a9726e699f142fa98ff891
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-09 20:37:14 -0400

    Updated: Button style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.scss';
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className='container-fluid'>
-      <div className='row'>
-        <div className='col-md-12'>
-          <header>
-            Welcome to Intergalactic Digital
-          </header>
-        </div>
+      <div class="jumbotron p-2 text-center">
+        <h1 class="display-4">Welcome to Intergalactic Digital</h1>
+        <p class="lead">Create Read Update Delete Blogs, Login and Logout. Users will have roles associated to them</p>
+        <hr class="my-4" />
+        <p>We are going to be able to login and see blogs and then improve as we develope more!</p>
+        
+        <button type="button" class="btn btn-primary btn-lg rounded-0">Learn more</button>
       </div>
     </div>
   );
