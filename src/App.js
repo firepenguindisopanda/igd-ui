ab6a8ec5828bc3fdf04c894f8c4a80a0b59b0868
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-09 21:36:27 -0400

    Added: Home,NavBar component and routes for home

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@
 import './custom.scss';
 import React from 'react';
 import './App.scss';
+import NavBar from './components/navbar/NavBar';
+import Home from './components/home/Home';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className='container-fluid'>
-      <div class="jumbotron p-2 text-center">
-        <h1 class="display-4">Welcome to Intergalactic Digital</h1>
-        <p class="lead">Create Read Update Delete Blogs, Login and Logout. Users will have roles associated to them</p>
-        <hr class="my-4" />
-        <p>We are going to be able to login and see blogs and then improve as we develope more!</p>
-        
-        <button type="button" class="btn btn-primary btn-lg rounded-0">Learn more</button>
+      <NavBar />
+      <Home />
+
+      <div className='container mt-3'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </div>
     </div>
   );
