b4593009ebe785058031e0bbb0d1906435d59f71
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-09 21:55:40 -0400

    Removed: Home duplicate Home component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import React from 'react';
 import './App.scss';
 import NavBar from './components/navbar/NavBar';
 import Home from './components/home/Home';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='container-fluid'>
       <NavBar />
-      <Home />
-
       <div className='container mt-3'>
         <Routes>
           <Route path='/' element={<Home />} />
