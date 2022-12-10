6ece6b463ed7f3a8d19e6d40910affc91228b26f
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-09 21:37:40 -0400

    Added: Home component and layout for Home page

---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div class="jumbotron p-2 text-center">
+            <h1 class="display-4">Welcome to Intergalactic Digital</h1>
+            <p class="lead">Create Read Update Delete Blogs, Login and Logout. Users will have roles associated to them</p>
+            <hr class="my-4" />
+            <p>We are going to be able to login and see blogs and then improve as we develope more!</p>
+
+            <button type="button" class="btn btn-primary btn-lg rounded-0">Learn more</button>
+        </div>
+    )
+}
+
+export default Home
