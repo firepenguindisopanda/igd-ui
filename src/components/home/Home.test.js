12fd0556f2f63a924a11f32d8fe2e718c6cb17b2
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-09 21:39:53 -0400

    Added: test for Home component and style

---
+++ src/components/home/Home.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
+
+test('renders heading with Welcome to Intergalactic Digital', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Welcome to Intergalactic Digital/i);
+  expect(linkElement).toBeInTheDocument();
+});
