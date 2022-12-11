9d4138610aad640448c86998a808b8c37918e199
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-10 20:25:23 -0400

    Added: the module for BlogDataService

---
+++ src/components/addblog/AddBlog.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import BlogDataService from '../../services/blogs.service';
+
+const AddBlog = () => {
+  return (
+    <div>AddBlog</div>
+  )
+}
+
+export default AddBlog
