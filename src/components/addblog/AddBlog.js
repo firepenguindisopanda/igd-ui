620e22f9de9b7e4fd49f1365781cd08fa9abe369
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-10 20:53:18 -0400

    Updated: the form to add a new blog with the logic

--- src/components/addblog/AddBlog.js
+++ src/components/addblog/AddBlog.js
@@ -2,8 +2,108 @@ import React, { useState } from 'react';
 import BlogDataService from '../../services/blogs.service';
 
 const AddBlog = () => {
+    const initialBlogState = {
+        id: null,
+        title: "",
+        description: "",
+        url: "",
+        published: false
+    };
+    const [blog, setBlog] = useState(initialBlogState);
+    const [submitted, setSubmitted] = useState(false);
+    const onChangeTitle = (e) => {
+        const title = e.target.value;
+        setBlog({...blog, title: title});
+    }
+    const onChangeDescription = (e) => {
+        const description = e.target.value;
+        setBlog({...blog, description: description});
+    }
+    const onChangeUrl = (e) => {
+        const url = e.target.value;
+        setBlog({...blog, url: url});
+    }
+
+    const saveBlog = () => {
+        let data = {
+            title: blog.title,
+            description: blog.description,
+            url: blog.url
+        };
+        BlogDataService.create(data).then(response => {
+            setBlog({
+                id: response.data.id,
+                title: response.data.title,
+                description: response.data.description,
+                url: response.data.url,
+                published: response.data.published
+            });
+        }).catch(e => {
+            console.log(e);
+        });
+        
+    }
+    const newBlog = () => {
+        setBlog(initialBlogState);
+        setSubmitted(false);
+    }
   return (
-    <div>AddBlog</div>
+    <div className='submit-form'>
+        {
+            submitted ? (
+                <div>
+                    <h4>You submitted successfully!</h4>
+                    <button className='btn btn-success' onClick={newBlog}>
+                        Add new blog
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    <div className='form-group'>
+                        <label htmlFor='title'>Title</label>
+                        <input 
+                        type='text'
+                        className='form-control'
+                        id='title'
+                        required
+                        value={blog.title}
+                        onChange={onChangeTitle}
+                        name='title'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='description'>Description</label>
+                        <input
+                        type='text'
+                        className='form-control'
+                        id='description'
+                        required
+                        value={blog.description}
+                        onChange={onChangeDescription}
+                        name="description"
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='url'>
+                            URL
+                        </label>
+                        <input
+                        type='text'
+                        className='form-control'
+                        id='url'
+                        required
+                        value={blog.url}
+                        onChange={onChangeUrl}
+                        name='url'
+                        />
+                    </div>
+                    <button className='btn btn-success' onClick={saveBlog}>
+                        Submit
+                    </button>
+                </div>
+            )
+        }
+    </div>
   )
 }
 
