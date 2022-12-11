3f9913cf9c728a1752c71fafea625942e77adcf7
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-10 21:00:04 -0400

    Added: route for addblog component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import './App.scss';
 import NavBar from './components/navbar/NavBar';
 import Home from './components/home/Home';
+import AddBlog from './components/addblog/AddBlog';
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <div className='container mt-3'>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path="/addblog" element={<AddBlog />} />
         </Routes>
       </div>
     </div>
