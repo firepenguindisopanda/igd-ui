862ffadb9c9b237282e25c4273736da6c0b394a7
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-10 20:21:18 -0400

    Updated: the data services for different routes

---
+++ src/services/blog.service.js
@@ -0,0 +1,27 @@
+import http from "../common/http-common";
+
+class BlogDataServices {
+    getAll() {
+        return http.get("/blogs");
+    }
+    get(id){
+        return http.get(`/blogs/${id}`);
+    }
+    create(data){
+        return http.post("/blogs", data);
+    }
+    update(id, data){
+        return http.put(`/blogs/${id}`, data);
+    }
+    delete(id){
+        return http.delete(`/blogs/${id}`);
+    }
+    deleteAll(){
+        return http.delete(`/blogs`);
+    }
+    findByTitle(title){
+        return http.get(`/blogs?title=${title}`);
+    }
+}
+
+export default new BlogDataServices();
