ad1b8b915666364054a36e6ac370addc46a702c8
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-11 22:20:12 -0400

    Added: portfolio file code

---
+++ src/pages/portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Portfolio = () => {
+    return (
+        <div className="jumbotron p-2 text-center">
+            
+        </div>
+    )
+}
+
+export default Portfolio
