0f3093759627f5e3d1f030a872b3e2db1369e6dd
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-13 17:47:11 -0400

    Updated: for postgresql

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,7 +1,8 @@
 export default function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
     if(user && user.accessToken){
-        return { Authorization: 'Bearer ' + user.accessToken };
+        // return { Authorization: 'Bearer ' + user.accessToken };
+        return { 'x-access-token': user.accessToken }; // for nodejs postgresql
     }else{
         return {};
     }
