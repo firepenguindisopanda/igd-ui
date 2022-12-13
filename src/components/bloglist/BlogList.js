d15a38707571955c0a3b827c88ba241adb27e1a5
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-13 00:07:44 -0400

    Added: import for scss

--- src/components/bloglist/BlogList.js
+++ src/components/bloglist/BlogList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import BlogDataService from '../../services/blog.service'
 import { Link } from 'react-router-dom'
+import './bloglist.scss'
 
 
 const BlogList = () => {
