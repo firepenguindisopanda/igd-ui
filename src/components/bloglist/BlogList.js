4e8bddc6ef90e31a6eb693db883afb09793fd981
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-12 23:27:54 -0400

    Added: state variables and functions for data

--- src/components/bloglist/BlogList.js
+++ src/components/bloglist/BlogList.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import BlogDataService from '../../services/blog.service'
+import { Link } from 'react-router-dom'
+
 
 const BlogList = () => {
+    const [blogs, setBlogs] = useState([])
+    const [currentBlog, setCurrentBlog] = useState(null)
+    const [currentIndex, setCurrentIndex] = useState(-1)
+    const [searchTitle, setSearchTitle] = useState('')
+
+    useEffect(() => {
+        retrieveBlogs();
+
+    }, []);
+
+    const onChangeSearchTitle = (e) => {
+        const searchTitle = e.target.value;
+        setSearchTitle(searchTitle);
+    }
+
+    const retrieveBlogs = () => {
+        BlogDataService.getAll().then(response => {
+            setBlogs(response.data);
+            console.log(response.data);
+        }).catch(e => {
+            console.log(e);
+        });
+    };
+
+    const refreshList = () => {
+        retrieveBlogs();
+        setCurrentBlog(null);
+        setCurrentIndex(-1);
+    }
+
+    const setActiveBlog = (blog, index) => {
+        setCurrentBlog(blog);
+        setCurrentIndex(index);
+    }
+
+    const removeAllBlogs = () => {
+        BlogDataService.removeAll().then(response => {
+            console.log(response.data);
+            refreshList();
+
+        }).catch(e => {
+            console.log(e);
+        });
+    }
+
+    const findByTitle = () => {
+        BlogDataService.findByTitle(searchTitle).then(response => {
+            setBlogs(response.data);
+            console.log(response.data);
+        }).catch(e => {
+            console.log(e);
+        });
+    }
+
+
   return (
-    <div>BlogList</div>
+    <div></div>
   )
 }
 
