bf7366723f1b69bc553b9df8d8dc80c77c198708
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-12 23:40:33 -0400

    Updated: code to display list of blogs and information if user clicks on one

--- src/components/bloglist/BlogList.js
+++ src/components/bloglist/BlogList.js
@@ -60,8 +60,90 @@ const BlogList = () => {
 
 
   return (
-    <div></div>
-  )
+    <div className='list row'>
+        <div className='col-md-8'>
+            <div className='input-group mb-3'>
+                <input
+                type="text"
+                className='form-control'
+                placeholder='Search by title'
+                value={searchTitle}
+                onChange={onChangeSearchTitle}
+                />
+                <div className='input-group-append'>
+                    <button 
+                    className='btn btn-outline-secondary' 
+                    type='button' 
+                    onClick={findByTitle}>
+                        Search for Blogs
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div className='col-md-6'>
+            <h4>Blogs List</h4>
+            <ul className='list-group'>
+                {
+                    blogs && blogs.map((blog, index) => (
+                        <li
+                        className={`list-group-item ${index === currentIndex ? 'active' : ''}`}
+                        onClick={() => setActiveBlog(blog, index)}
+                        key={index}
+                        >
+                            {blog.title}
+                        </li>
+                    ))
+                }
+            </ul>
+            <button
+            className='m-3 btn btn-sm btn-danger'
+            onClick={removeAllBlogs}
+            >
+                Remove All Blogs
+            </button>
+        </div>
+        <div
+        className='col-md-6'
+        >
+            {
+                currentBlog ? (
+                    <div>
+                        <h4>Blog</h4>
+                        <div>
+                            <label>
+                                <strong>Title:</strong>
+                            </label>{' '}
+                            {currentBlog.title}
+                        </div>
+                        <div>
+                            <label>
+                                <strong>Description:</strong>
+                            </label>{' '}
+                            {currentBlog.description}
+                        </div>
+                        <div>
+                            <label>
+                                <strong>Status:</strong>
+                            </label>{' '}
+                            {currentBlog.published ? 'Published' : 'Pending'}
+                        </div>
+                        <Link
+                        to={'/blogs/' + currentBlog.id}
+                        className='badge badge-warning'
+                        >
+                            Edit Blog
+                        </Link>
+                    </div>
+                ) : (
+                    <div>
+                        <br />
+                        <p>Please click on a Blog...</p>
+                    </div>
+                )
+            }
+        </div>
+    </div>
+  );
 }
 
 export default BlogList
