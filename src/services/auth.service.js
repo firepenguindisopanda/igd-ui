4828f9e33e43f74f021444b6f48d5709a48981e1
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-13 17:43:36 -0400

    Updated: with AuthService functions

---
+++ src/services/auth.service.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:3000/api/auth/';
+
+class AuthService {
+    async login(username, password) {
+        const response = await axios.post(API_URL + 'signin', { username, password });
+        if (response.data.accessToken) {
+            localStorage.setItem('user', JSON.stringify(response.data));
+        }
+        return response.data;
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+    }
+
+    async register(username, email, password) {
+        const response = await axios.post(API_URL + 'signup', { username, email, password });
+        return response.data;
+    }
+}
+
+export default new AuthService();
