b34d594e80de32fa982a4ad639f51f1efb0c3e74
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-14 20:36:29 -0400

    Updated: home page to get public content

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import UserService from '../../services/user.service'
 
 const Home = () => {
+    const [content, setContent] = useState('');
+    useEffect(() => {
+        UserService.getPublicConent().then(
+            (response) => {
+                setContent(response.data);
+            },
+            (error) => {
+                const _content = (error.response && error.response.data) || error.message || error.toString();
+                setContent(_content);
+            }
+        );
+    }, []);
+
     return (
         <div className="jumbotron p-2 text-center">
             <h1 className="display-4">Welcome to Intergalactic Digital</h1>
@@ -9,6 +23,11 @@ const Home = () => {
             <p>We are going to be able to login and see blogs and then improve as we develope more!</p>
 
             <button type="button" className="btn btn-primary btn-lg rounded-0">Learn more</button>
+            <div className='row'>
+                <div className='col-md-12'>
+                    {content}
+                </div>
+            </div>
         </div>
     )
 }
