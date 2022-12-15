64e03902be434c918bfb89472cb99121fbdc074e
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-14 20:12:56 -0400

    Updated: pages to render component

---
+++ src/pages/login/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from '../../components/loginform/LoginForm'
+
+
+const LoginPage = () => {
+  return (
+    <div className='container'>
+        <Login />
+    </div>
+  )
+}
+
+export default LoginPage
