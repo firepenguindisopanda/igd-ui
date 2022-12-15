8374823f53f4f3ce44cdd0cc8ddb644893d1d626
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-14 23:15:52 -0400

    Added: form container with placeholder img

--- src/components/registerform/RegisterForm.js
+++ src/components/registerform/RegisterForm.js
@@ -88,7 +88,15 @@ const RegisterForm = () => {
 
 
   return (
-    <div>RegisterForm</div>
+    <div className='col-md-12'>
+      <div className='card card-container'>
+        <img
+          src='//ssl.gstatic.com/accounts/ui/avatar_2x.png'
+          alt='profile-img'
+          className='profile-img-card'
+        />
+      </div>
+    </div>
   )
 }
 
